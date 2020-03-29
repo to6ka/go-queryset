051c9388711b5b3142ce2337116c857ca29818bc
Anton Vakhmistrov <a.vakhmistrov@corp.mail.ru>
2020-03-29 17:32:48 +0300

    smart file detection

--- cmd/goqueryset/goqueryset.go
+++ cmd/goqueryset/goqueryset.go
@@ -3,23 +3,35 @@ package main
 import (
 	"flag"
 	"log"
+	"os"
 	"path"
 	"strings"
 
 	"github.com/to6ka/go-queryset/queryset"
 )
 
+var outFile = flag.String("out", "{in}_queryset.go", "path to output file")
+
 func main() {
-	inFile := flag.String("in", "models.go", "path to input file")
-	outFile := flag.String("out", "{in}_queryset.go", "path to output file")
 	flag.Parse()
 
-	if strings.Contains(*outFile, "{in}") {
-		baseName := path.Base(*inFile)
-		*outFile = strings.Replace(*outFile, "{in}", baseName, 1)
+	inFile := os.Getenv("GOFILE")
+	inFile = path.Base(inFile)
+
+	fileStat, err := os.Stat(inFile)
+	if err != nil {
+		log.Fatalf("failed to access file %s", inFile)
+	}
+	if !fileStat.Mode().IsRegular() {
+		log.Fatalf("file %s is not a regular file", inFile)
 	}
 
-	if err := queryset.GenerateQuerySets(*inFile, *outFile); err != nil {
+	if strings.Contains(*outFile, "{in}") {
+		inName := strings.TrimSuffix(inFile, ".go")
+		*outFile = strings.Replace(*outFile, "{in}", inName, 1)
+	}
+
+	if err := queryset.GenerateQuerySets(inFile, *outFile); err != nil {
 		log.Fatalf("can't generate query sets: %s", err)
 	}
 }
